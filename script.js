10837fc2ed5bbac4cb96c08df130f42228923af0
Wojciech <wojciech.zarem@gmail.com>
2024-03-29 16:53:24 +0100

    first comments

--- script.js
+++ script.js
@@ -46,87 +46,87 @@ function adjustLastExpanderLower() {
     }
 }
 
-// UPPER EXPANDER ADJUSTMENTS (ARROWS):
-const search = document.getElementsByClassName("searchButton")[0]
-const expTop = document.getElementsByClassName("expTop")[0]
+// // UPPER EXPANDER ADJUSTMENTS (ARROWS):
+// const search = document.getElementsByClassName("searchButton")[0]
+// const expTop = document.getElementsByClassName("expTop")[0]
 
 
-const removedFromRight = []
-const removedFromLeft = []
+// const removedFromRight = []
+// const removedFromLeft = []
 
-const arrowExpander = document.createElement("div")
-const arrowIcon = document.createElement("span")
-arrowExpander.classList.add("expanderElement", "expElemTop")
-arrowIcon.classList.add("expandIndicator", "rotateToRight", "arrowIcon")
-arrowIcon.setAttribute("id", "arrowIcon")
-arrowExpander.setAttribute("id", "arrow")
-arrowExpander.append(arrowIcon)
+// const arrowExpander = document.createElement("div")
+// const arrowIcon = document.createElement("span")
+// arrowExpander.classList.add("expanderElement", "expElemTop")
+// arrowIcon.classList.add("expandIndicator", "rotateToRight", "arrowIcon")
+// arrowIcon.setAttribute("id", "arrowIcon")
+// arrowExpander.setAttribute("id", "arrow")
+// arrowExpander.append(arrowIcon)
 
-function adjustLastExpanderUpper() {
-    const searchMargin = getComputedStyle(search).getPropertyValue("margin-left")
-    const marginNum = Number(searchMargin.slice(0, -2))
+// function adjustLastExpanderUpper() {
+//     const searchMargin = getComputedStyle(search).getPropertyValue("margin-left")
+//     const marginNum = Number(searchMargin.slice(0, -2))
 
-    const nodeLast = expTop.lastElementChild
-    const nodeWidth = nodeLast?.clientWidth
-    const prevSibl = nodeLast.previousElementSibling
-    const prevWidth = prevSibl?.clientWidth
-    const lastRemoved = removedFromRight[removedFromRight.length - 1]?.[0]
-    const lastRemovedWidth = removedFromRight[removedFromRight.length - 1]?.[1]
+//     const nodeLast = expTop.lastElementChild
+//     const nodeWidth = nodeLast?.clientWidth
+//     const prevSibl = nodeLast.previousElementSibling
+//     const prevWidth = prevSibl?.clientWidth
+//     const lastRemoved = removedFromRight[removedFromRight.length - 1]?.[0]
+//     const lastRemovedWidth = removedFromRight[removedFromRight.length - 1]?.[1]
 
-    arrowExpander.style.position = "absolute"
-    arrowExpander.style.order = "1"
-    arrowIcon.classList.remove("rotateToLeft")
+//     arrowExpander.style.position = "absolute"
+//     arrowExpander.style.order = "1"
+//     arrowIcon.classList.remove("rotateToLeft")
 
-    if (marginNum <= 0 + arrowExpander.clientWidth && nodeLast) { 
-        console.log("now")
-        if (nodeLast.id === "arrow" && prevSibl) {
-            removedFromRight.push([prevSibl, prevWidth])
-            expTop.removeChild(prevSibl)
-        } else if (nodeLast.id !== "arrow") {
-            removedFromRight.push([nodeLast, nodeWidth])
-            expTop.removeChild(nodeLast)
-            expTop.appendChild(arrowExpander)
-        }
-    } else if (marginNum > 0 && marginNum > lastRemovedWidth + 8 + 34 && removedFromRight.length >= 2) {
-        expTop.insertBefore(lastRemoved, nodeLast)
-        removedFromRight.pop()
-    } 
-    else if (marginNum > 0 && removedFromRight.length < 2 && marginNum > lastRemovedWidth + 8) { // 8 is a column gap number in px
-        console.log("last")
-        if (nodeLast.id === "arrow") expTop.removeChild(nodeLast)
-        expTop.appendChild(lastRemoved)
-        removedFromRight.pop()
-    }
-}
+//     if (marginNum <= 0 + arrowExpander.clientWidth && nodeLast) { 
+//         console.log("now")
+//         if (nodeLast.id === "arrow" && prevSibl) {
+//             removedFromRight.push([prevSibl, prevWidth])
+//             expTop.removeChild(prevSibl)
+//         } else if (nodeLast.id !== "arrow") {
+//             removedFromRight.push([nodeLast, nodeWidth])
+//             expTop.removeChild(nodeLast)
+//             expTop.appendChild(arrowExpander)
+//         }
+//     } else if (marginNum > 0 && marginNum > lastRemovedWidth + 8 + 34 && removedFromRight.length >= 2) {
+//         expTop.insertBefore(lastRemoved, nodeLast)
+//         removedFromRight.pop()
+//     } 
+//     else if (marginNum > 0 && removedFromRight.length < 2 && marginNum > lastRemovedWidth + 8) { // 8 is a column gap number in px
+//         console.log("last")
+//         if (nodeLast.id === "arrow") expTop.removeChild(nodeLast)
+//         expTop.appendChild(lastRemoved)
+//         removedFromRight.pop()
+//     }
+// }
 
-// ARROW FUNCTIONALITY
-const arrUpNodes = Array.from(document.querySelectorAll(".expElemTop")).map(elem => [elem, elem.clientWidth])
-const sumExpWidth = arrUpNodes.reduce((a,c)=> a + c[1] + 8, -8) // -8 because there are fewer gaps than elements
+// // ARROW FUNCTIONALITY
+// const arrUpNodes = Array.from(document.querySelectorAll(".expElemTop")).map(elem => [elem, elem.clientWidth])
+// const sumExpWidth = arrUpNodes.reduce((a,c)=> a + c[1] + 8, -8) // -8 because there are fewer gaps than elements
 
-function handleArrowClick() {
-    let availableSpace = 0
-    const searchMarginNum = Number(getComputedStyle(search).getPropertyValue("margin-left").slice(0, -2))
-    const elems = Array.from(document.querySelectorAll(".expElemTop"))
-    const elsWidth = elems.reduce((a,c) => a + c.clientWidth,  searchMarginNum)
-    availableSpace = elsWidth - 2 * arrowExpander.clientWidth
-    console.log("searchMarginNum " + searchMarginNum)
-    console.log(elems)
-    console.log("elsWidth " + elsWidth)
-    console.log("avaible space " + availableSpace)
-   // const removedNumber = 
-    const arrowPos = getComputedStyle(arrowExpander).getPropertyValue("position")
-    // if (arrowPos === "static") { // CLICK TO LEFT
-    //     arrowExpander.style.position = "absolute"
-    //     arrowExpander.style.order = "1"
-    //     arrowIcon.classList.remove("rotateToLeft")
-    // } 
-    // else
-     if (arrowPos === "absolute") { // CLICK TO RIGHT
-       // arrowExpander.style.position = "static"
-        // arrowExpander.style.order = "0"
-        // arrowIcon.classList.add("rotateToLeft")
-    }
-}
+// function handleArrowClick() {
+//     let availableSpace = 0
+//     const searchMarginNum = Number(getComputedStyle(search).getPropertyValue("margin-left").slice(0, -2))
+//     const elems = Array.from(document.querySelectorAll(".expElemTop"))
+//     const elsWidth = elems.reduce((a,c) => a + c.clientWidth,  searchMarginNum)
+//     availableSpace = elsWidth - 2 * arrowExpander.clientWidth
+//     console.log("searchMarginNum " + searchMarginNum)
+//     console.log(elems)
+//     console.log("elsWidth " + elsWidth)
+//     console.log("avaible space " + availableSpace)
+//    // const removedNumber = 
+//     const arrowPos = getComputedStyle(arrowExpander).getPropertyValue("position")
+//     // if (arrowPos === "static") { // CLICK TO LEFT
+//     //     arrowExpander.style.position = "absolute"
+//     //     arrowExpander.style.order = "1"
+//     //     arrowIcon.classList.remove("rotateToLeft")
+//     // } 
+//     // else
+//      if (arrowPos === "absolute") { // CLICK TO RIGHT
+//        // arrowExpander.style.position = "static"
+//         // arrowExpander.style.order = "0"
+//         // arrowIcon.classList.add("rotateToLeft")
+//     }
+// }
 
 function shouldResize(n = 0) {
     if (n >= 10) throw new Error('something went wrong');
